7c12f4018fd0d26775a3d2e6ba5736e744c1ba53
mursalin@gmail.com <MURSA>
2023-06-10 19:52:24 +0600

    added the user get  api

--- index.js
+++ index.js
@@ -79,15 +79,15 @@ async function run() {
   });
 
   // 3 PATCH USERS
-  // app.patch("/users/admin/:id", async (req, res) => {
-  //   const id = req.params.id;
-  //   console.log(id);
-  //   const filter = { _id: new ObjectId(id) };
-  //   const updateDoc = {
-  //     $set: {
-  //       role: "admin",
-  //     },
-  //   };
+  app.patch("/users/admin/:id", async (req, res) => {
+    const id = req.params.id;
+    console.log(id);
+    const filter = { _id: new ObjectId(id) };
+    const updateDoc = {
+      $set: {
+        role: "admin",
+      },
+    };
 
     const result = await usersCollection.updateOne(filter, updateDoc);
     res.send(result);
