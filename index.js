a31b20bf61b36dc0e61932a918fcc2bb15eb57ae
mursalin@gmail.com <MURSA>
2023-06-11 01:42:00 +0600

    modify the full code now

--- index.js
+++ index.js
@@ -1,84 +1,143 @@
 const express = require("express");
 const cors = require("cors");
+const jwt = require("jsonwebtoken");
 require("dotenv").config();
-const app = express();
-const port = process.env.PORT || 3000;
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 
-// Middleware >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
-app.use(cors());
-app.use(express.json());
-// MONGODB CODE >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
-
+const app = express();
+const port = process.env.PORT || 3000;
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.1nqrclq.mongodb.net/?retryWrites=true&w=majority`;
 
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
-const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  },
-});
-//important note : remove try function before vercel deploy
-async function run() {
-  // MongoDB database and collection ....
-  const classesCollection = client.db("summerCamp").collection("classes");
-  const usersCollection = client.db("summerCamp").collection("users");
-  const cartCollection = client.db("summerCamp").collection("cart");
+app.use(cors());
+app.use(express.json());
 
-  // cart api zone starts....>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
-  //1. POST/CREATE FROM HERE...
-  app.post("/cart", async (req, res) => {
-    const user = req.body;
-    console.log("new user", user);
-    const result = await cartCollection.insertOne(user);
+const verifyJWT = (req, res, next) => {
+  const authorization = req.headers.authorization;
+  if (!authorization) {
+    return res.status(401).send({ error: true, message: "Unauthorized access" });
+  }
+  const token = authorization.split(" ")[1];
+
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+    if (err) {
+      return res.status(401).send({ error: true, message: "Unauthorized access" });
+    }
+    req.decoded = decoded;
+    next();
+  });
+};
+
+async function run() {
+  const client = new MongoClient(uri, {
+    serverApi: {
+      version: ServerApiVersion.v1,
+      strict: true,
+      deprecationErrors: true,
+    },
+  });
+
+  try {
+    await client.connect();
+    console.log("Connected to MongoDB success!");
+  } catch (error) {
+    console.error("Failed to connect to MongoDB:", error);
+    return;
+  }
+
+  const db = client.db("summerCamp");
+  const classesCollection = db.collection("classes");
+  const usersCollection = db.collection("users");
+  const cartCollection = db.collection("cart");
+
+  app.get("/users/admin/:email", verifyJWT, async (req, res) => {
+    const email = req.params.email;
+
+    if (req.decoded.email !== email) {
+      return res.send({ admin: false });
+    }
+
+    const query = { email: email };
+    const user = await usersCollection.findOne(query);
+    const result = { admin: user?.role === "admin" };
     res.send(result);
   });
-  //2.  GET /READ FROM HERE......
-  app.get("/cart", async (req, res) => {
+
+  app.post("/jwt", (req, res) => {
+    const user = req.body;
+    const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+      expiresIn: "3h",
+    });
+    res.send({ token });
+  });
+
+  const verifyAdmin = async (req, res, next) => {
+    const email = req.decoded.email;
+    const query = { email: email };
+    const user = await usersCollection.findOne(query);
+    if (user?.role !== "admin") {
+      return res.status(403).send({ error: true, message: "Forbidden access" });
+    }
+    next();
+  };
+
+  app.post("/cart", async (req, res) => {
+    const cartItem = req.body;
+    console.log("New cart item", cartItem);
+
+    const { email, className } = cartItem;
+
+    const query = { email: email, className: className };
+    const existingCartItem = await cartCollection.findOne(query);
+
+    if (existingCartItem) {
+      return res.send({ message: "Item already exists in the cart" });
+    }
+
+    const result = await cartCollection.insertOne(cartItem);
+    res.send(result);
+  });
+
+  app.get("/cart", verifyJWT, async (req, res) => {
     const email = req.query.email;
     if (!email) {
-      res.send([]);
+      return res.send([]);
+    }
+    const decodedEmail = req.decoded.email;
+    if (email !== decodedEmail) {
+      return res.status(403).send({ error: true, message: "Forbidden access" });
     }
     const query = { email: email };
     const result = await cartCollection.find(query).toArray();
     res.send(result);
   });
 
-  //4. DELETE FROM HERE .....
   app.delete("/cart/:id", async (req, res) => {
     const id = req.params.id;
-    console.log("deleting ", id);
+    console.log("Deleting ", id);
     const query = { _id: new ObjectId(id) };
     const result = await cartCollection.deleteOne(query);
     res.send(result);
   });
-  // >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> cart api zone starts....>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 
-  // >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> users api zone starts....>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
-  //1. POST/CREATE USERS FROM HERE...
   app.post("/users", async (req, res) => {
     const user = req.body;
-    console.log("new user", user);
+    console.log("New user", user);
     const query = { email: user.email };
     const existingUser = await usersCollection.findOne(query);
     console.log(existingUser);
     if (existingUser) {
-      return res.send({ massage: "user already exists" });
+      return res.send({ message: "User already exists" });
     }
     const result = await usersCollection.insertOne(user);
     res.send(result);
   });
 
-  //2.  GET /READ FROM USERS HERE......
-  app.get("/users", async (req, res) => {
+  app.get("/users", verifyJWT, verifyAdmin, async (req, res) => {
     const cursor = usersCollection.find();
     const result = await cursor.toArray();
     res.send(result);
   });
 
-  // 3 PATCH USERS
   app.patch("/users/admin/:id", async (req, res) => {
     const id = req.params.id;
     console.log(id);
@@ -93,8 +152,6 @@ async function run() {
     res.send(result);
   });
 
-
-// 4 DELETE USERS 
   app.delete("/users/:id", async (req, res) => {
     const id = req.params.id;
     const filter = { _id: new ObjectId(id) };
@@ -111,68 +168,16 @@ async function run() {
       res.status(500).send({ success: false, message: "An error occurred" });
     }
   });
-  // >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> users api zone Ends....>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
-  //2.  GET /READ FROM HERE......
+
   app.get("/classes", async (req, res) => {
     const cursor = classesCollection.find();
     const result = await cursor.toArray();
     res.send(result);
   });
 
-  //3. UPDATE FROM HERE .....................................................,,
-  app.get("/task/:id", async (req, res) => {
-    const id = req.params.id;
-    console.log("updating ", id);
-    const query = { _id: new ObjectId(id) };
-    const result = await theCollection.findOne(query);
-    res.send(result);
+  app.listen(port, () => {
+    console.log(`Server is running on port: ${port}`);
   });
-
-  app.put("/task/:id", async (req, res) => {
-    const id = req.params.id;
-    const user = req.body;
-    console.log("updating user", id);
-    const filter = { _id: new ObjectId(id) };
-    const options = { upsert: true };
-    const updatedUser = {
-      $set: {
-        title: user.title,
-        description: user.description,
-        status: user.status,
-        date: user.date,
-      },
-    };
-    const result = await theCollection.updateOne(filter, updatedUser, options);
-    res.send(result);
-  });
-  // patch the task status
-  app.patch("/task/:id", async (req, res) => {
-    const id = req.params.id;
-    const { status } = req.body;
-    const filter = { _id: new ObjectId(id) };
-    const update = { $set: { status } };
-    const result = await theCollection.updateOne(filter, update);
-    res.send(result);
-  });
-
-  //4. DELETE FROM HERE .....
-  app.delete("/task/:id", async (req, res) => {
-    const id = req.params.id;
-    console.log("deleting ", id);
-    const query = { _id: new ObjectId(id) };
-    const result = await theCollection.deleteOne(query);
-    res.send(result);
-  });
-  // Send a ping to confirm a successful connection
-  await client.db("admin").command({ ping: 1 });
-  console.log("Pinged your deployment. You successfully connected to MongoDB!");
 }
-run().catch(console.dir);
 
-app.get("/", (req, res) => {
-  res.send(" Alhamdulillah summer camp running so hot ");
-});
-// starting the server>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
-app.listen(port, () => {
-  console.log(` summer running so hot ${port} port`);
-});
+run().catch(console.dir);
